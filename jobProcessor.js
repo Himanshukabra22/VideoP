a49e0021ff9ff060db71b5e2ae274def9fe8f066
Himanshu Kabra <himanshukabra2212@gmail.com>
2023-05-22 23:01:16 +0530

    bug fixes 1

--- jobProcessor.js
+++ jobProcessor.js
@@ -1,7 +1,7 @@
-const Queue = require('bull');
-const { exec } = require('child_process');
+const Queue = require("bull");
+const { exec } = require("child_process");
 
-const videoQueue = new Queue('videoQueue', 'redis://localhost:6379');
+const videoQueue = new Queue("videoQueue", "redis://localhost:6379");
 
 function getDirectDownloadLink(driveLink) {
   const regex = /\/d\/(.+?)\/(?:view|edit|export)/;
@@ -14,33 +14,38 @@ function getDirectDownloadLink(driveLink) {
   return null; // Return null if the input is not a valid Google Drive link
 }
 
-
 videoQueue.process(async (job) => {
+  try {
+    let videoLink = job.data.videoLink;
+    videoLink = getDirectDownloadLink(videoLink);
 
-  let videoLink = job.data.videoLink;
-      videoLink = getDirectDownloadLink(videoLink);
+    // Execute FFmpeg command for noise reduction
+    const outputFileName = `processed_${Date.now()}.mkv`;
+    const inputVideo = `input-video_${Date.now()}.mkv`;
 
-  // Execute FFmpeg command for noise reduction
-  const outputFileName = `processed_${Date.now()}.mkv`;
-  const inputVideo = `input-video_${Date.now()}.mkv`
+    const command = `mkdir -p input_files && mkdir -p output_files && wget --timeout=300 -c -O ./input_files/${inputVideo} "${videoLink}" && ffmpeg -i ./input_files/${inputVideo} -af "highpass=f=200, lowpass=f=3000, afftdn=nf=-25" ./output_files/${outputFileName}`;
 
-  const command = `wget -c -O ${inputVideo} "${videoLink}" && ffmpeg -i ${inputVideo} -af "highpass=f=200, lowpass=f=3000, afftdn=nf=-25" ${outputFileName}`;
-
-  // Execute the FFmpeg command
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error processing video: ${error}`);
-      job.moveToFailed({ message: 'Video processing failed.' }, true);
-    } else {
-      console.log(`Video processed successfully: ${outputFileName}`);
-      job.moveToCompleted({ processedVideo: outputFileName }, true);
-    }
-    exec(`rm ${inputVideo}`);
-  });
-  // Return a promise to indicate the completion of the job processing
-  return new Promise((resolve,reject) => {
-    resolve();
-  });
+    // Execute the FFmpeg command
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error processing video: ${error}`);
+        job.moveToFailed({ message: "Video processing failed." }, true);
+      } else {
+        console.log(`Video processed successfully: ${outputFileName}`);
+        job.moveToCompleted({ processedVideo: outputFileName }, true);
+      }
+      exec(`rm ./input_files/${inputVideo}`);
+    });
+    // Return a promise to indicate the completion of the job processing
+    return new Promise((resolve, reject) => {
+      resolve();
+    });
+  } catch (error) {
+    console.log(`Video is not processed due to error : ${error}`);
+    return new Promise((resolve, reject) => {
+      reject();
+    });
+  }
 });
 
 module.exports = videoQueue;
--- server.js
+++ server.js
@@ -1,53 +1,95 @@
-const express = require('express');
+const express = require("express");
+const fs = require('fs');
+
 const app = express();
 const port = 3000;
-const videoQueue = require('./jobProcessor');
+const videoQueue = require("./jobProcessor");
 
 app.use(express.json());
 
+// function checkFileExists(filePath) {
+//   try {
+//     fs.accessSync(filePath, fs.constants.F_OK);
+//     return true; // File exists
+//   } catch (err) {
+//     return false; // File does not exist
+//   }
+// }
 // Enable CORS for SSE
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
-app.get('/',(req,res) => {
-  res.send("Hello!!");
-})
 
-app.get('/jobs/:jobId', async (req, res) => {
-  const jobId = req.params.jobId;
-  const job = await videoQueue.getJob(jobId);
-  console.log(job);
-  if (!job) {
-    res.status(404).json({ status: 'error', message: 'Job not found.' });
-    return;
-  }
-  
-  if(job.failedReason !== undefined)
-  {
-    res.json({status : "not ok"});
-  }
-  if (job.isCompleted()) {
-    res.json({ status: 'ok', processedVideo: job.returnvalue.processedVideo });
-    return;
-  }
-// Job is still in progress
-res.json({ status: 'progress', message: 'Job is still in progress.' });
+app.get("/", (req, res) => {
+  res.send("Hello!!");
 });
 
-app.post('/jobs', async (req, res) => {
-  const videoLink = req.body.videoLink;
+app.get("/jobs/:jobId", async (req, res) => {
+  const jobId = req.params.jobId;
+  try {
+    const job = await videoQueue.getJob(jobId);
 
-  const job = await videoQueue.add({ videoLink });
-if(job)
-{
-  res.json({ status: 'ok', jobId: job.id });
-}
-else
-{
-  res.json({ status: 'not ok', jobId: job.id });
-}
+    if (!job) {
+      res.status(404).json({ status: "not ok", message: "Job not found." });
+      return;
+    }
+
+    // console.log("DEBUG OPEN");
+    // console.log(job);
+    // console.log("DEBUG CLOSE");
+
+    // const jobProgress = await job.progress();
+    // console.log(`Job progress : ${jobProgress}`);
+
+    if (job.failedReason !== undefined) {
+      res.json({ status: "not ok", message : "Job processing failed."});
+    }
+
+    const jobFinished = await job.finished();
+    console.log(`Job finished : ${jobFinished}`);
+
+    if(jobFinished!==null)
+    {
+      const isCompletedJob = await job.isCompleted();
+      // console.log(`isCompletedJob : ${isCompletedJob}`);
+      if (isCompletedJob) {
+        console.log("JOB COMPLETED");
+        res.json({
+          status: "ok",
+          message : "Job processing completed.",
+          processedVideo: job.returnvalue.processedVideo,
+        });
+        return;
+      }
+    }
+    res.json({
+      status: "ok",
+      message : "Job is under process."
+    });
+    return;
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "ok", message: "Job is still in progress." });
+  }
+});
+
+app.post("/jobs", async (req, res) => {
+  const videoLink = req.body.videoLink;
+  try {
+    const job = await videoQueue.add({ videoLink });
+    if (job) {
+      res.json({ status: "ok", jobId: job.id });
+    } else {
+      res.json({ status: "not ok" });
+    }
+  } catch (error) {
+    res.json({ status: "not ok", error });
+  }
 });
 
 app.listen(port, () => {
